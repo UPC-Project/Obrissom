c3b760ccbbe82f279cba3e803850c4b376d73c8d
Melina Morales <melina.morales@mi.unc.edu.ar>
2026-08-28 16:27:51 -0300

    feature: enemy health bar deactivates when player is too far away

--- Assets/Scripts/Enemy/EnemyUI.cs
+++ Assets/Scripts/Enemy/EnemyUI.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 
 public class EnemyUI : NetworkBehaviour
 {
-    [SerializeField] Image _HealthBar;
+    [SerializeField] Image _healthBarImage;
+    [SerializeField] GameObject _healthBar;
 
     [Rpc(SendTo.Everyone, InvokePermission = RpcInvokePermission.Everyone)]
     public void UpdateHealthUIRpc(float amount, float maxHealth)
     {
-        _HealthBar.fillAmount = amount / maxHealth;
+        _healthBarImage.fillAmount = amount / maxHealth;
+    }
+
+    public void SetHealthBarActive(bool isActive)
+    {
+        _healthBar.SetActive(isActive);
     }
 }
